b450b92a90b6ee6569a4b21cddd1750437660320
mao <1296193245@qq.com>
2022-10-09 15:07:03 +0800

    android_StartService

--- app/src/main/java/mao/android_startservice/MainActivity.java
+++ app/src/main/java/mao/android_startservice/MainActivity.java
@@ -2,7 +2,9 @@ package mao.android_startservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +14,27 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent();
+        intent.setAction("mao.android_startservice.service.MYSERVICE");
+        intent.setPackage("mao.android_startservice");
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startService(intent);
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                stopService(intent);
+            }
+        });
     }
 }
